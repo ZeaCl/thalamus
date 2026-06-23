5637986ed8a7fd85e4f721dc9051c8d5a1570bb1
Carlos Hinostroza <chinostroza@gmail.com>
2026-06-23 16:56:03 -0400

    chore: remove unrelated SDK build artifacts from PR

--- sdk/dist/hooks/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { U as UseAdminOptions, h as UseAdminReturn, i as UseThalamusOptions, j as UseThalamusReturn, u as useAdmin, m as useThalamus } from '../index-C5rsqdqK.js';
-import '../index-BnHWPrKX.js';
